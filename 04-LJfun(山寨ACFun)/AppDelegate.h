de78567bef8979695f656463369c0949398c65bc
Asuka <154906186@qq.com>
2016-03-31 01:24:20 +0800

    Initial Commit

---
+++ 04-LJfun(山寨ACFun)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-LJfun(山寨ACFun)
+//
+//  Created by  a on 16/3/31.
+//  Copyright © 2016年 eva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 04-LJfun(山寨ACFun)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  04-LJfun(山寨ACFun)
+//
+//  Created by  a on 16/3/31.
+//  Copyright © 2016年 eva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
